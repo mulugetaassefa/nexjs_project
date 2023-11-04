f525caa66c0f9553fe7260ea2708f53b71bae213
@mulugetaassefa! <mulugetaassefa077@gmail.com>
2023-11-04 12:10:39 -0800

    create issue model

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+'use client';
+
+import React, { use } from 'react';
+import { usePathname  } from 'next/navigation';
 import Link from 'next/link';
 import { AiFillBug } from 'react-icons/ai';
+import classnames from 'classnames';
+
 const NavBar = () => {
+   const currentPath = usePathname();
+   console.log(currentPath);
 
     const links = [
         { lable: 'Dashboard', href: '/'},
-        { lable: 'Issues', href: '/Issue'}
-    ]
+        { lable: 'Issues', href: '/Issues'}
+    ];
+
   return (
     <nav  className='flex space-x-6 border-b mb-5 h-14  items-center'>
       <Link href="/" > <AiFillBug /> </Link>
@@ -14,7 +22,11 @@ const NavBar = () => {
         {links.map(link => 
         <Link 
         key={link.href}
-        className=' text-zinc-500 hover:text-zinc-800 ' 
+        className= { classnames({
+          'text-zink-900': link.href ===currentPath,
+          'text-zink-500': link.href !== currentPath,
+          'hover: text-zink-800  transition-colors': true,
+        })}
         href={link.href}>{link.lable}
         </Link>
         
